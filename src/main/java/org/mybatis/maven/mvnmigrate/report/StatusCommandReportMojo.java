13bc0b468e2a9313cf91821fb902a8801d941688
Jeremy Landis <jeremylandis@hotmail.com>
2025-05-26 17:44:20 -0400

    Move site rendenderer to jsr 330

--- src/main/java/org/mybatis/maven/mvnmigrate/report/StatusCommandReportMojo.java
+++ src/main/java/org/mybatis/maven/mvnmigrate/report/StatusCommandReportMojo.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2010-2024 the original author or authors.
+ *    Copyright 2010-2025 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+
+import javax.inject.Inject;
 
 import org.apache.ibatis.migration.Change;
 import org.apache.ibatis.migration.commands.StatusCommand;
@@ -71,7 +73,7 @@ public final class StatusCommandReportMojo extends AbstractMavenReport {
   /**
    * The project site renderer.
    */
-  @Component(role = Renderer.class)
+  @Inject
   private Renderer siteRenderer;
 
   /**
